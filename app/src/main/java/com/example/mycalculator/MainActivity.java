c2a70cdbc5556e38dbc319b750b4565fcda5d869
janbronicki@gmail.com <Jan Bronicki>
2021-04-27 03:08:16 +0200

    have mercy

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.mycalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView previousCalculation;
+    private EditText display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        previousCalculation = findViewById(R.id.previousCalculationView);
+        display = findViewById(R.id.displayEditText);
     }
+
+
+
 }
